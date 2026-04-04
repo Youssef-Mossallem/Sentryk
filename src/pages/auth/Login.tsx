9922bf451673626fa6d5c10573bcc625d7a725ea
YoussefMossallem <ymslm120@gmail.com>
2026-04-04 02:31:38 +0200

    Update Login.tsx

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -23,7 +23,7 @@ export default function Login() {
   const navigate = useNavigate();
   
   // قراءة الرابط من .env أو الافتراضي 3000
-  const API_BASE_URL = "https://sentrykapi-86unvpa1.b4a.run";
+  const API_BASE_URL = "https://sentrykapi-l4vi8x0h.b4a.run";
 
   // States
   const [showPassword, setShowPassword] = useState(false);
